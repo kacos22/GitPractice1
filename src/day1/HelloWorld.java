ca208079342a5e7721c69edcac59250c61449d87
KarlosMartinez <k4martinez98@gmail.com>
2021-04-24 16:52:46 -0400

    added more work after publishing to github

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 
         System.out.println("Hello World");
         System.out.println("Adding GitHub now");
+        System.out.println("YAY MY STUFF IS ON GITHUB");
     }
 }
